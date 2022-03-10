ba4023e5f78f06f18aceafaf28b9f341d138510d
Christopher Burke <cburke@sjcny.edu>
2022-03-10 14:46:29 -0500

    Initial commit.

---
+++ src/main/java/com/cburke/examples/demo/HelloWorldApplication.java
@@ -0,0 +1,23 @@
+package com.cburke.examples.demo;
+
+import java.util.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello,", "World.");
+	}
+
+}
